16509627d886c4b65637cd2fa896c2581d2faa54
Lesya-Brodskaya <110602282+Lesya-Brodskaya@users.noreply.github.com>
2023-05-15 14:05:38 +0300

    import PersistGate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from ' redux-persist/integration/react ';
+import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import App from 'components/App';
 import './index.css';
